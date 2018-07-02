b133cbc9440c9ecaca18568a3bd8ef3dfa395e05
Ryan Boland <bolandryanm@gmail.com>
2018-07-02 09:08:07 -0400

    update libsass

--- test/native_test.rb
+++ test/native_test.rb
@@ -9,7 +9,7 @@ module SassC
 
     class General < MiniTest::Test
       def test_it_reports_the_libsass_version
-        assert_equal "3.4.3", Native.version
+        assert_equal "3.5.4", Native.version
       end
     end
 
